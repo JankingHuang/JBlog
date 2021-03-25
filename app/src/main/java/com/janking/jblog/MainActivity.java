1b958eb52caa1c683cff1157cdd09c44039d0f30
Janking <archkernel@outlook.com>
2021-03-25 16:18:03 +0800

    实现内部加载网页

--- app/src/main/java/com/janking/jblog/MainActivity.java
+++ app/src/main/java/com/janking/jblog/MainActivity.java
@@ -9,8 +9,14 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.security.Permission;
@@ -18,16 +24,70 @@ import java.security.Permission;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private WebSettings webSettings ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
         startIntenet();
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.canGoBack();
+        webView.canGoForward();
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+               MainActivity.this.setTitle(title);
+            }
+        });
+
+//        webView.setWebViewClient(new WebViewClient(){
+//            @Override
+//            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//                super.onPageStarted(view, url, favicon);
+//                Toast.makeText(MainActivity.this, "博客加载中....", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                Toast.makeText(MainActivity.this, "博客加载完成!!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//        });
+
     }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     private void loadWeb(){
        webView.loadUrl("http://jankinghuang.gitee.io");
     }
+
+
 
     private void startIntenet(){
         if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET)
